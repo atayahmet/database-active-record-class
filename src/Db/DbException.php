2ebdfc2c4356bb21600a38e986999bed4f41f221
Ahmet <ahmet.atay@hotmail.com>
2014-11-17 00:32:31 +0200

    Exception class refactoring

--- src/Db/DbException.php
+++ src/Db/DbException.php
@@ -1,6 +1,14 @@
 <?php namespace Db;
 
 class DbException extends \Exception implements DbExceptionInterface {
+	protected static $dbErrMsg = array(
+		'incorrect_parm' => 'Incorrect parameter',
+		'update_batch_missing_parameter' => 'Missing passed parameters to the update method',
+		'update_batch_ref_col_err' => 'Referance colum not specified',
+		'table_name' => 'Table name not found',
+		'table_name_incorrect' => 'Incorrect table name'
+	);
+
 	public static function fire($parm = false)
 	{
 		if(is_array($parm)){
@@ -9,7 +17,16 @@ class DbException extends \Exception implements DbExceptionInterface {
 			return $view;
 		}
 	}
-	
+
+	public static function errorMsg($key = false)
+	{
+		if(isset(self::$dbErrMsg[$key])){
+			return self::$dbErrMsg[$key];
+		}
+
+		return '';
+	}
+
 	public static function injectView($file = false, $parm = false)
 	{
 		ob_start();
