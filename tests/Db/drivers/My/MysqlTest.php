b883ca778c60070cd403c81125e2e62bfef104a2
Ahmet <ahmet.atay@hotmail.com>
2014-11-21 16:21:32 +0200

    TestCase development

--- tests/Db/drivers/My/MysqlTest.php
+++ tests/Db/drivers/My/MysqlTest.php
@@ -2,12 +2,34 @@
 
 namespace Db;
 
-include __DIR__ . '/../../../../../../../vendor/autoload.php';
 use \Db\Query as DB;
 
 class MysqlTest extends \PHPUnit_Framework_TestCase {
-	public function testAdd()
+	public function testInsert()
 	{
-		var_dump(DB::get('langs'));
+		// ilk senaryomuz tabloya yeni kayıt oluşturma
+		// kullanıcı tablosuna bir yeni üye ekliyoruz ve ardın etkilenen kayıt sayısını
+		// UnitTest'e gönderiyoruz
+		DB::insert('members',
+			array(
+			'name' 	=> 'Ahmet',
+			'age'	=> 18
+			)
+		);
+
+		// sonuç istediğimiz gibimi kontrol ediyoruz.
+		$this->assertGreaterThan(0, DB::insert_id());
 	}
+
+	public function testUpdate()
+	{
+		// ikinci senaryomuz update metodu.
+		// Tablodaki ilk kaydı guncelleyeceğiz.
+		// Sonrasında tekrar etkilenen satır sayısı alacağız.
+		$result = DB::select('*')->order_by('id','asc')->get('members');
+		var_dump($result->result());
+	
+	}
+
+
 }
